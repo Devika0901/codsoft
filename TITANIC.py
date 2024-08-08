d80fdc2b9279ca313bba29679c775da8fd2277a6
Devika0901 <69295622+Devika0901@users.noreply.github.com>
2024-08-08 23:37:03 +0530

    Update TITANIC.py

--- TITANIC.py
+++ TITANIC.py
@@ -1,1 +1,5 @@
-
+#Use the Titanic dataset to build a model that predicts whether a passenger on the Titanic survived or not.
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
