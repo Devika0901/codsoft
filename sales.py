3b2928ba2924a0e8dafe858490ee98a9b6dfa827
Devika0901 <69295622+Devika0901@users.noreply.github.com>
2024-08-15 22:25:10 +0530

    Create sales.py

---
+++ sales.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+d=pd.read_csv("advertising.csv")
+d.head()
+d.isnull().sum()
+d.describe()
+sns.pairplot(d, x_vars=['TV', 'Newspaper', 'Radio'], y_vars='Sales', height=5, aspect=1, kind='scatter')
+plt.show()
+d.corr()
+sns.heatmap(d.corr(),annot=True)
+plt.show()
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import statsmodels.api as sm
+X = d['TV']
+y = d['Sales']
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 100)
+X_train_sm = sm.add_constant(X_train)
+lr = sm.OLS(y_train, X_train_sm).fit()
+lr.summary()
+
